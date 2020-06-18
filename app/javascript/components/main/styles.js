2aa31cda5dcbf0ebe7f4ff1f861d4decca3b354a
luifermoron <contact@luifermoron.com>
2020-06-18 04:25:41 -0400

    add style property

--- app/javascript/components/main/styles.js
+++ app/javascript/components/main/styles.js
@@ -1,3 +1,3 @@
-export const container = { display: 'flex', flex: 1, width: "100%", height: 1000 };
+export const container = { display: 'flex', flex: 1, width: "100%", height: 1000, justifyContent: 'space-evenly' };
 export const column = { display: 'flex', flexDirection: 'column' };
 export const row = { display: 'flex', flexDirection: 'row' };
